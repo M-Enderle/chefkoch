0a9ff85f6f766abb732c2aa2aae5ba487f5f3181
THDMoritzEnderle <private@moritzenderle.com>
2021-11-01 17:41:32 +0100

    updated recipe retrieval to use the hidden dict

---
+++ tests/test_chefkoch_api.py
@@ -0,0 +1,10 @@
+from chefkoch_api import chefkoch
+
+print(chefkoch.get_specifications())
+recipes = chefkoch.get_specific(search_term="Lasagne",
+                                filters={"page": 2, "sort": "rating", "duration": 30,
+                                                    "difficulty": {"easy": True, "medium": True, "hard": False}},
+                                specifications=["vegetarisch", "gemuese"])
+print(recipes)
+
+
